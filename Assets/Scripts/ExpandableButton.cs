c0b5fdef891b96504bcce0aebe9c8f49661ebdb7
NakataNoGyaku <demertknight@gmail.com>
2019-05-08 19:42:17 +0900

    Expandable Button added.

---
+++ Assets/Scripts/ExpandableButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ExpandableButton : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] TextMeshProUGUI text;
+
+    [SerializeField] bool expanded;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ExpandButton()
+    {
+        expanded = true;
+        animator.SetBool ("pressed", true);
+    }
+
+    public void ShrinkButton()
+    {
+        expanded = false;
+        animator.SetBool ("pressed", false);
+    }
+
+    public void ChangeText()
+    {
+        
+        text.text = (expanded == true ? "Menu" : "Button");
+        
+    }
+}
