4197b240e39c0ea4bcad40755211146a92dbab84
NakataNoGyaku <demertknight@gmail.com>
2019-05-20 20:17:17 +0900

    Radial button 1/3 setup

---
+++ Assets/Scripts/RadialButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RadialButton : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] bool expanded;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ExpandButton()
+    {
+        expanded = true;
+        //animator.SetBool ("pressed", true);
+    }
+
+    public void ShrinkButton()
+    {
+        expanded = false;
+        //animator.SetBool ("pressed", false);
+    }
+}
